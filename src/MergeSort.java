82c6f1c9bf7a1d18fa7a5ce97925a2c89ccc76bb
System-in-a-System <textconstruction@protonmail.com>
2020-07-08 11:31:03 +0300

    Adding recursive method to carry out merge sort on the array of strings

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 
 public class MergeSort {
 	
@@ -7,9 +8,6 @@ public class MergeSort {
 	// Merge sort algorithm divides the array into two halves 
 	// and applies a merge sort on each half recursively.
 	// After the two halves are sorted, they are to be merged.
-			
-	// (Resources used as a starting point for the algorithm: 
-	// http://web.cs.iastate.edu/~smkautz/cs227f16/examples/week11/MergeSort.java
 	public static int[] mergeSort(int[] in) {
 			
 		
@@ -77,6 +75,81 @@ public class MergeSort {
 
 			return arrCopy;
 			
+	}
+	
+	
+	
+	// Recursive method to carry out merge sort on the array of strings
+	
+	// Merge sort algorithm divides the array into two halves 
+	// and applies a merge sort on each half recursively.
+	// After the two halves are sorted, they are to be merged.
+				
+	public static String[] mergeSort(String[] in, Comparator<String> c) {
+			
+		// Create a safe copy of the input array (original input array shall not be mutated)
+		String[] arrCopy = new String[in.length];
+					
+		for (int i = 0; i < arrCopy.length; i++) 
+			arrCopy[i] = in[i];
+
+					
+		// Set the base case to terminate the recursion
+		if (arrCopy.length <= 1) 
+			return arrCopy;
+		
+
+					
+		// Locate the middle point and divide the current array in halves
+		int middlePoint = arrCopy.length / 2;
+		String[] arr1 = new String[middlePoint];
+		String[] arr2 = new String[arrCopy.length - arr1.length];
+
+		// Populate the first half
+		for (int i = 0; i < arr1.length; i++) 
+			arr1[i] = arrCopy[i];
+					
+
+		// Populate the second half
+		for (int i = 0; i < arr2.length; i++) 
+			arr2[i] = arrCopy[middlePoint++];
+					
+
+		// Recursively divide & update arr1 and arr2
+		arr1 = mergeSort(arr1, c); 
+		arr2 = mergeSort(arr2, c);
+
+					
+					
+		// Sort & Merge subarrays back into a common array:
+					
+		// Set initial indexes for arrays
+		int index1 = 0; // index for arr1 
+		int index2 = 0; // index for arr2
+		int index = 0; // index for common gathering arrCopy
+
+		// Compare & Sort & Save into arrCopy
+		while (index1 < arr1.length && index2 < arr2.length) {
+				
+			if (c.compare(arr1[index1], arr2[index2]) < 0) 
+				arrCopy[index++] = arr1[index1++];
+							
+			else 
+				arrCopy[index++] = arr2[index2++];
+		
+		}
+				
+			
+		// Take care of possible remnants
+		while (index1 < arr1.length) 
+			arrCopy[index++] = arr1[index1++];
+			
+
+		while (index2 < arr2.length) 
+			arrCopy[index++] = arr2[index2++];
+			
+
+		return arrCopy;
 	}
 	
 	
